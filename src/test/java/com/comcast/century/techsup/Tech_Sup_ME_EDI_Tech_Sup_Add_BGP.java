4c3ba8eb6c67ae6a72a9746d6a4298952138ce8a
Jatin Luthra <Jatin_Luthra@cable.comcast.com>
2016-09-29 21:16:26 +0530

    updated some test methods

--- src/test/java/com/comcast/century/techsup/Tech_Sup_ME_EDI_Tech_Sup_Add_BGP.java
+++ src/test/java/com/comcast/century/techsup/Tech_Sup_ME_EDI_Tech_Sup_Add_BGP.java
@@ -8,7 +8,7 @@ import com.comcast.century.cm.pages.ProcessTabPageCM;
 public class Tech_Sup_ME_EDI_Tech_Sup_Add_BGP extends ServiceFlow_Techsup {
 
 	@Test(priority = 30000)
-	public void addBGP() throws InterruptedException {
+	public void addBGPToEDI() throws InterruptedException {
 		new FeatureTabPageCM(frameworkContext).clickOnFeatureTab();
 		new FeatureTabPageCM(frameworkContext).selectBGP();
 		new ProcessTabPageCM(frameworkContext).BGPConfiguration();
--- src/test/java/com/comcast/century/techsup/Tech_Supp_Add_BGP_to_EDI_PRI_Service.java
+++ src/test/java/com/comcast/century/techsup/Tech_Supp_Add_BGP_to_EDI_PRI_Service.java
@@ -2,16 +2,12 @@ package com.comcast.century.techsup;
 
 import org.testng.annotations.Test;
 
-import com.comcast.century.cm.pages.FeatureTabPageCM;
-import com.comcast.century.cm.pages.ProcessTabPageCM;
 
-public class Tech_Supp_Add_BGP_to_EDI_PRI_Service extends ServiceFlow_Techsup {
+
+public class Tech_Supp_Add_BGP_to_EDI_PRI_Service extends Tech_Sup_ME_EDI_Tech_Sup_Add_BGP {
 
 	@Test(priority = 30000)
-	public void addBGP() throws InterruptedException {
-		new FeatureTabPageCM(frameworkContext).clickOnFeatureTab();
-		new FeatureTabPageCM(frameworkContext).selectBGP();
-		new ProcessTabPageCM(frameworkContext).BGPConfiguration();
-		new ProcessTabPageCM(frameworkContext).ClickOnContinueButton();
+	public void addBGPToEDI_PRI() throws InterruptedException {
+		addBGPToEDI();
 	}
 }
