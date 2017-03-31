5dc3bb4aae1951a95a961d34898529a3e9241378
ohegde001c <ohegde001c@PATAPL-335559.cable.comcast.com>
2017-03-31 09:47:27 -0400

    Updated template to include latest changes to framework-core

---
+++ src/main/java/com/comcast/pages/CommonLocators.java
@@ -0,0 +1,15 @@
+package com.comcast.pages;
+
+/**
+ *
+ * Locator class for all the common web elements across all application pages.
+ * Extend the locator class from this class to access these locIDs. Update
+ * locators in CommonObjects.csv.
+ *
+ */
+
+public class CommonLocators {
+
+    // Eg:
+    // public static final String txtUserName = "txtUserName";
+}
